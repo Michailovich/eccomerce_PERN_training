ba5634e6cb386d90706c83bef5b12f11cdc8891d
SergeyKuligin <gosobes@gmail.com>
2022-03-26 21:08:58 +0400

    http

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -6,40 +6,40 @@ import {createDevice, fetchBrands, fetchDevices, fetchTypes} from "../../http/de
 import {observer} from "mobx-react-lite";
 
 const CreateDevice = observer(({show, onHide}) => {
-    const {device} = useContext(Context)
-    const [name, setName] = useState('')
-    const [price, setPrice] = useState(0)
-    const [file, setFile] = useState(null)
-    const [info, setInfo] = useState([])
+    const {device} = useContext(Context);
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState(0);
+    const [file, setFile] = useState(null);
+    const [info, setInfo] = useState([]);
 
     useEffect(() => {
-        fetchTypes().then(data => device.setTypes(data))
-        fetchBrands().then(data => device.setBrands(data))
+        fetchTypes().then(data => device.setTypes(data));
+        fetchBrands().then(data => device.setBrands(data));
     }, [])
 
     const addInfo = () => {
-        setInfo([...info, {title: '', description: '', number: Date.now()}])
+        setInfo([...info, {title: '', description: '', number: Date.now()}]);
     }
     const removeInfo = (number) => {
         setInfo(info.filter(i => i.number !== number))
     }
     const changeInfo = (key, value, number) => {
-        setInfo(info.map(i => i.number === number ? {...i, [key]: value} : i))
+        setInfo(info.map(i => i.number === number ? {...i, [key]: value} : i));
     }
 
     const selectFile = e => {
-        setFile(e.target.files[0])
+        setFile(e.target.files[0]);
     }
 
     const addDevice = () => {
-        const formData = new FormData()
-        formData.append('name', name)
-        formData.append('price', `${price}`)
-        formData.append('img', file)
-        formData.append('brandId', device.selectedBrand.id)
-        formData.append('typeId', device.selectedType.id)
-        formData.append('info', JSON.stringify(info))
-        createDevice(formData).then(data => onHide())
+        const formData = new FormData();
+        formData.append('name', name);
+        formData.append('price', `${price}`);
+        formData.append('img', file);
+        formData.append('brandId', device.selectedBrand.id);
+        formData.append('typeId', device.selectedType.id);
+        formData.append('info', JSON.stringify(info));
+        createDevice(formData).then(data => onHide());
     }
 
     return (
